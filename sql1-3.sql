75a509d2c5a2a32374844676e35f2eec59990960
JL-stefan <zeyongma@163.com>
2024-11-12 13:52:48 +0800

    Create sql1-3.sql

---
+++ sql1-3.sql
@@ -0,0 +1,12 @@
+-- 自治事务开始
+PROCEDURE TRANS_C AS
+DECLARE
+  v_value INT;
+BEGIN
+  --自治事务更新同一数据
+  UPDATE test_table SET value = value + 1 WHERE id = 1;
+  --获取更新后的值
+  SELECT value INTO v_value FROM test_table WHERE id = 1;
+  -输出自治事务更新后的值
+  DBMS_OUTPUT.PUT_LINE('自治事务更新后的值:' || v_value);
+END;
